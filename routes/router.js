019078d8635dca7e58ab46582d1fa74874526ec8
grillopress <grillopress@gmail.com>
2018-05-28 22:43:38 +0100

    update to new kit route handling

--- routes/router.js
+++ routes/router.js
@@ -16,22 +16,6 @@ router.get('/cookie', function(req, res, next) {
   res.render( 'cookie/index' );
 });
 
-// GET all v1 URL reqs and push them to a template in the v1 file
-// This feels really brittle and hacky...
-// No handling of no view found...
-router.get('/cookie/:view', function(req, res, next) {
-  var theView = req.params.view;
-  res.render( 'cookie/' + theView );
-});
-
-router.get('/cookie/:subdir/:view', function(req, res, next) {
-  var theView = req.params.view;
-  var theDir = req.params.subdir;
-  res.render( 'cookie/' + theDir + '/' + theView );
-});
-
-
-
 // GET v1 index page.
 router.get('/v1', function(req, res, next) {
   res.render( 'v1/index' );
@@ -40,22 +24,28 @@ router.get('/v1', function(req, res, next) {
 // GET all v1 URL reqs and push them to a template in the v1 file
 // This feels really brittle and hacky...
 // No handling of no view found...
-router.get('/v1/:view', function(req, res, next) {
+router.get('/:view', function(req, res, next) {
   var theView = req.params.view;
-  res.render( 'v1/' + theView );
+  res.render( theView );
 });
 
-router.get('/v1/:subdir/:view', function(req, res, next) {
+router.get('/:view/', function(req, res, next) {
+  var theView = req.params.view;
+  res.render( theView + '/index.html' );
+});
+
+router.get('/:subdir/:view', function(req, res, next) {
   var theView = req.params.view;
   var theDir = req.params.subdir;
-  res.render( 'v1/' + theDir + '/' + theView );
+  res.render( theDir + '/' + theView );
 });
 
-router.get('/v1/:subdir/:subdir2/:view', function(req, res, next) {
+router.get('/:subdir/:subdir2/:view', function(req, res, next) {
   var theView = req.params.view;
   var theDir = req.params.subdir;
   var theDir2 = req.params.subdir2;
-  res.render( 'v1/' + theDir + '/' + theDir2 + '/' + theView );
+  res.render( theDir + '/' + theDir2 + '/' + theView );
 });
+
 
 module.exports = router;
